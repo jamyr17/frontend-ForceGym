089fcffc9cf48791783950c7b131c35aacb211a2
Jamyr González <josuegonza1702@gmail.com>
2025-03-14 23:27:07 -0600

    [14/03 23:20h] Se puede visualizar perfectamente la vista de Plantillas con los componentes correspondientes

--- src/TemplateNotification/Filter.tsx
+++ src/TemplateNotification/Filter.tsx
@@ -1,9 +1,96 @@
+import { IoFilterOutline } from "react-icons/io5";
+import useProductInventoryStore from "./Store";
+import { MdOutlineCancel } from "react-icons/md";
+import { useCommonDataStore } from "../shared/CommonDataStore";
 
+export function FilterButton() {
+    const { filterByStatus, filterByNotificationType, showModalFilter } = useProductInventoryStore()
+    const filteringStyles = (
+        filterByStatus!='' || filterByNotificationType!=0
+    ) && ' bg-white outline-none'
 
-export const FilterButton = () => {
-
+    return (
+        <button
+            className={"flex items-center gap-4 text-lg uppercase outline-2 py-2 px-4 rounded-lg hover:cursor-pointer hover:bg-slate-300" + filteringStyles}
+            onClick={()=>{ showModalFilter() }}
+        >
+            <IoFilterOutline />
+            <span>Filtrar</span>
+        </button>
+    );
 }
 
-export const FilterSelect = () => {
+export function FilterSelect() {
+    const { 
+        filterByStatus, 
+        filterByNotificationType,
+        changeFilterByStatus, 
+        changeFilterByNotificationType
+    } = useProductInventoryStore()
+    const filteredStatusSelectStyles = filterByStatus!='' && ' px-0.5 rounded-md border-2 border-yellow text-yellow'
+    const filteredNotificationTypeStyles = filterByNotificationType!=0 && ' px-0.5 border-2 border-yellow text-yellow'
+    const { notificationTypes } = useCommonDataStore();
 
+    return (
+        <div className="flex flex-col gap-4">
+            {/* Filtro por Estado */}
+            <div className="flex items-center gap-4">
+                <label htmlFor="status" className="w-20">Estado</label>
+                <select
+                    className={'border rounded-md p-2 w-52 text-center' + filteredStatusSelectStyles}
+                    name="status"
+                    id="status"
+                    value={filterByStatus} 
+                    onChange={(e) => {
+                        if(Number(e.target.value) === 0){
+                            changeFilterByStatus('')
+                        }else{
+                            changeFilterByStatus(e.target.value)
+                        }
+                    }}
+                >
+                    <option value={0}> Activos </option>
+                    <option value={'Inactivos'}> Inactivos </option>
+                    <option value={'Todos'}> Todos </option>
+                </select>
+                {filterByStatus && 
+                    <button
+                        className="text-2xl text-yellow"
+                        onClick={() => changeFilterByStatus('')}
+                    >
+                        <MdOutlineCancel className="hover:cursor-pointer" />
+                    </button>
+                }
+            </div>
+    
+            {/* Filtro por Tipo de Notificacion*/}
+            <div className="flex items-center gap-4">
+                <label htmlFor="status" className="w-20">Tipo de Notificación</label>
+                <select
+                    className={'border rounded-md p-2 w-52 text-center' + filteredNotificationTypeStyles}
+                    name="notificationType"
+                    id="notificationType"
+                    value={filterByNotificationType} 
+                    onChange={(e) => changeFilterByNotificationType(+e.target.value)}
+                >
+                    <option value={0}>Todos</option>
+
+                    {notificationTypes.map((noti)=> (
+                        <option key={noti.idNotificationType} value={noti.idNotificationType}>
+                            {noti.name}
+                        </option>
+                    ))}
+                </select>
+                {filterByNotificationType && 
+                    <button
+                        className="text-2xl text-yellow"
+                        onClick={() => changeFilterByNotificationType(0)}
+                    >
+                        <MdOutlineCancel className="hover:cursor-pointer" />
+                    </button>
+                }
+            </div>
+        </div>
+    );      
 }
+
