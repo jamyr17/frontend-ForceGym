f97219ff9da4aa6d221230a4ccb61e6b5e63359e
Jamyr González <135465554+jamyr17@users.noreply.github.com>
2025-03-29 15:44:05 -0600

    [FOR-348 29/03 15:00h] Implementación de recaptcha en el login (#45)

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -1,23 +1,26 @@
+import { useRef } from "react"
 import type { CredencialUser } from "../shared/types/index"
 import PasswordInput from "../shared/components/PasswordInput";
+import ReCAPTCHA from 'react-google-recaptcha'
 
 type LoginProps = {
     credencialUser: CredencialUser
     setCredencialUser: React.Dispatch<React.SetStateAction<CredencialUser>>
-    handleLoginSubmit: (e: any) => void
+    handleLoginSubmit: (e: any, refReCaptcha: React.RefObject<ReCAPTCHA>) => void
 }
 
 function Login ({credencialUser, setCredencialUser, handleLoginSubmit} : LoginProps) {
+    const recaptcha = useRef<ReCAPTCHA>(null)
 
     return (
         <div className="flex flex-col justify-center items-center h-screen bg-black">
             <main className="flex justify-center h-[550px] gap-16 p-12 w-auto bg-white rounded-lg">
-                <section className="flex flex-col h-full gap-12">
+                <section className="flex flex-col h-full gap-4">
                     <header className="flex justify-center">
-                        <img src="Logo.jpg" alt="Logo de Force GYM" className="w-64 h-auto"></img>
+                        <img src="Logo.jpg" alt="Logo de Force GYM" className="w-52 h-auto"></img>
                     </header>
 
-                    <form className="flex flex-col gap-4" onSubmit={handleLoginSubmit}>
+                    <form className="flex flex-col gap-4" onSubmit={(e) => handleLoginSubmit(e, recaptcha)}>
                         <label htmlFor='username' className="text-1xl font-bold">Usuario</label>
                         <input 
                             type="text" 
@@ -32,17 +35,19 @@ function Login ({credencialUser, setCredencialUser, handleLoginSubmit} : LoginPr
 
                         <label htmlFor="password" className="text-1xl font-bold">Contraseña</label>
                         <PasswordInput
-                        id="password"
-                        name="password"
-                        value={credencialUser.password}
-                        onChange={e => setCredencialUser({ ...credencialUser, password: e.target.value })}
-                        placeholder="Ingresar contraseña"
-                        required
+                            id="password"
+                            name="password"
+                            value={credencialUser.password}
+                            onChange={e => setCredencialUser({ ...credencialUser, password: e.target.value })}
+                            placeholder="Ingresar contraseña"
+                            required
                         />
 
+                        <ReCAPTCHA ref={recaptcha} sitekey={`${import.meta.env.VITE_RECAPTCHA_SITE_KEY}`} />
 
-                        <button type="submit" className="text-1xl mt-8 py-2 bg-black text-white rounded-4xl transition-all hover:bg-yellow hover:text-black hover:cursor-pointer">Iniciar Sesión</button>
+                        <button type="submit" className="text-1xl mt-4 py-2 bg-black text-white rounded-4xl transition-all hover:bg-yellow hover:text-black hover:cursor-pointer">Iniciar Sesión</button>
                         <a href="#" className="text-1xls text-center hover:underline hover:decoration-yellow">¿Olvidó su contraseña?</a>
+                
                     </form>
                 </section>
                 <aside className="bg-yellow self-center h-[470px] w-[375px] rounded-2xl">
--- src/Login/useLogin.ts
+++ src/Login/useLogin.ts
@@ -4,9 +4,10 @@ import Swal from 'sweetalert2';
 import { postData } from "../shared/services/gym"
 import { setAuthHeader, setAuthUser } from "../shared/utils/authentication"
 import { CredencialUser } from "../shared/types"
+import ReCAPTCHA from "react-google-recaptcha";
 
 export const useLogin = () => {
-    const [credencialUser, setCredencialUser] = useState<CredencialUser>({username: '', password: ''})
+    const [credencialUser, setCredencialUser] = useState<CredencialUser>({username: '', password: '', recaptchaToken: ''})
     const navigate = useNavigate()
 
     useEffect(() => {
@@ -14,9 +15,26 @@ export const useLogin = () => {
         setAuthUser(null)
     }, [])
 
-    const handleLoginSubmit = async (e:any) => {
+    const handleLoginSubmit = async (e:any, refReCaptcha: React.RefObject<ReCAPTCHA>) => {
         e.preventDefault()
 
+        const captchaValue = refReCaptcha.current?.getValue()
+        if (!captchaValue) {
+            await Swal.fire({
+                title: 'Acceso no autorizado',
+                text: 'Debe completar el ReCaptcha',
+                icon: 'error',
+                confirmButtonText: 'OK',
+                timer: 3000,
+                timerProgressBar: true,
+                width: 300,
+                confirmButtonColor: '#CFAD04'
+            })
+
+            return
+        }
+
+        credencialUser.recaptchaToken = captchaValue
         const res = await postData(`${import.meta.env.VITE_URL_API}login`, credencialUser)
 
         if(res.data.loggedUser){
